4935ebf94319c6b47153ec5564f3295948fb2ed2
Jim Clark <jim.clark@generalassemb.ly>
2022-05-27 10:19:53 -0700

    Finish Part 2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function NavBar() {
   return (
-    <nav>NavBar</nav>
+    <nav>
+      <Link to="/orders">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+    </nav>
   );
 }
