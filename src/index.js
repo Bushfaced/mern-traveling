57a342fe296ce09d41ea499a7f0f732df8f74534
Jim Clark <jim.clark@generalassemb.ly>
2022-05-27 10:11:30 -0700

    Add NavBar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
 // import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,7 @@ import App from './pages/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router><App /></Router>
   </React.StrictMode>
 );
 
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
 
   return (
     <main className="App">
       { user ?
-        <NewOrderPage />
+        <>
+          <NavBar />
+          <Routes>
+            {/* Route components in here */}
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
         :
         <AuthPage />
       }
