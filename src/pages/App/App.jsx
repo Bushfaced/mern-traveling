d4231da75990968c33c715711be7bc7e7afb31cf
Jim Clark <jim.clark@generalassemb.ly>
2022-05-27 09:33:04 -0700

    Add page-level components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <main className="App">
-      App
+      { user ?
+        <NewOrderPage />
+        :
+        <AuthPage />
+      }
     </main>
   );
 }
