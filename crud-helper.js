e6165e97e4d3a91b0bca70f817839b719d1db47b
richardguzman <thisisrichardguzman@gmail.com>
2022-06-08 14:31:00 -0700

    at least the navbar changed now

--- crud-helper.js
+++ crud-helper.js
@@ -4,7 +4,7 @@ require('./config/database');
 
 // Require the Mongoose models
 const User = require('./models/user');
-const Item = require('./models/log');
+const Log = require('./models/log');
 // const Category = require('./models/category');
 // const Order = require('./models/order');
 
