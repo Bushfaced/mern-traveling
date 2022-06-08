79166ab0ebdfcf0ceaeef151e76a6d2f43747ec7
richardguzman <thisisrichardguzman@gmail.com>
2022-06-08 15:13:55 -0700

    babystepping log form

--- models/logSchema.js
+++ models/logSchema.js
@@ -2,7 +2,7 @@ const Schema = require('mongoose').Schema;
 
 const logSchema = new Schema({
   title: { type: String, required: true },
-  location: { type: String, required: true },
+  destination: { type: String, required: true },
   content: { type: String, required: true },
 }, {
   timestamps: true
--- src/components/AddLogForm/AddLogForm.jsx
+++ src/components/AddLogForm/AddLogForm.jsx
@@ -1,10 +1,12 @@
-export default function AddLogForm() {
+export default function AddLogForm({ log, handleAddLog }) {
   return (
     <main>
       <div><input type="text" placeholder="Title"/></div>
       <div><input type="text" placeholder="Destination"/></div>
       <div><textarea name="content"></textarea></div>
-      <button>Add Log</button>
+      <button className="btn-sm" onClick={() => handleAddLog(log._id)}>
+        ADD
+      </button>
     </main>
   );
 }
