7f56492c09a36db99d199c943ce065cb57cb0419
richardguzman <thisisrichardguzman@gmail.com>
2022-06-11 18:40:46 -0700

    collapsed view

--- src/components/LogCard/LogCard.jsx
+++ src/components/LogCard/LogCard.jsx
@@ -5,11 +5,11 @@ export default function LogCard({log, handleDeleteLog, user}) {
     <main className="accordion accordion-flush">
       <div className="card">
         <div className="accordion-item">
-          <h4 className="accordion-header" id="panelsStayOpen-headingOne">
-            <button id="accordion-button" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse" aria-expanded="true" aria-controls="flush-collapseOne">{log.title}
+          <h4 className="accordion-header">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse" aria-expanded="true" aria-controls="flush-collapseOne">{log.title}
             </button>
           </h4>
-          <div id="flush-collapse" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne">
+          <div id="flush-collapse" className="accordion-collapse collapse" aria-labelledby="flush-headingOne">
             <div className="accordion-body">
               <h6 className="card-title">{log.destination}</h6>
               <br />
