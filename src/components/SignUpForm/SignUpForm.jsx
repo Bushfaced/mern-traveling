4f8567cab52933412ecdcf39379031db7f2f2762
richardguzman <thisisrichardguzman@gmail.com>
2022-06-11 18:22:10 -0700

    fixed image repeating

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -37,7 +37,7 @@ export default class SignUpForm extends Component {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
-        <div className="">
+        <div className="signUpForm">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <label>Name</label>
             <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
